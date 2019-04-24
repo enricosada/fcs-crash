ca6f9a910925d0cf1eca4cba0fd1f7588b372dd2
Enrico Sada <enrico.sada@jet.com>
2019-04-24 17:40:09 +0200

    add example repro

---
+++ Program.fs
@@ -0,0 +1,71 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Compiler.SourceCodeServices
+open Dotnet.ProjInfo.Workspace
+open Dotnet.ProjInfo.Workspace.FCS
+open System.IO
+
+let parseAndTypeCheck projPath =
+
+    let msbuildLocator = MSBuildLocator()
+    let loader =
+        LoaderConfig.Default msbuildLocator
+        |> Loader.Create
+
+    let netFwInfo =
+        NetFWInfoConfig.Default msbuildLocator
+        |> NetFWInfo.Create
+
+    let fcs =
+
+      let checker =
+        FSharpChecker.Create(
+          projectCacheSize = 200,
+          keepAllBackgroundResolutions = true,
+          keepAssemblyContents = true)
+
+      checker.ImplicitlyStartBackgroundWork <- true
+
+      checker
+
+    let fcsBinder = FCSBinder(netFwInfo, loader, fcs)
+
+    printfn "Loading %s" projPath
+
+    loader.LoadProjects [ projPath ]
+
+    let fcsPoOpt = fcsBinder.GetProjectOptions(projPath)
+
+    let fcsPo = fcsPoOpt |> Option.get
+
+    printfn "FcsPo: %A" fcsPo
+
+    printfn "running ParseAndCheckProject ..."
+
+    let result =
+      fcs.ParseAndCheckProject(fcsPo)
+      |> Async.RunSynchronously
+
+    if result.Errors.Length > 0 then
+      printfn "FCS Result errors: %A" result.Errors
+    else
+
+      printfn "running GetAllUsesOfAllSymbols ..."
+
+      let uses =
+        result.GetAllUsesOfAllSymbols()
+        |> Async.RunSynchronously
+
+      printfn "Usages: %A" uses
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    let projPath = Path.GetFullPath "repro/reproducer.fsproj"
+
+    parseAndTypeCheck projPath
+
+    0 // return an integer exit code
